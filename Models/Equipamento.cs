b04675cce72dc098a4801efd7e454f3d7388a36a
Gabriel Matiolli <gabrielmatiollif@gmail.com>
2025-07-01 10:55:32 -0300

    Add validation attributes to Equipamento model for Nome property

--- Models/Equipamento.cs
+++ Models/Equipamento.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace gr2_api.Models
 {
     public class Equipamento
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nome é obrigatório.")]
+        [StringLength(100, ErrorMessage = "Nome deve ter no máximo 100 caracteres.")]
         public string Nome { get; set; }
     }
 }
